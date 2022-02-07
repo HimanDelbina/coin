da1300324ebd187ec0f8d7e815a748e55dbcca65
HimanDelbina <himandelbina@gmail.com>
2022-02-07 23:31:04 +0330

    First project upload

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -26,7 +26,6 @@ import 'Coin/Chart/olhc_page.dart';
 import 'News/news.dart';
 import 'filter_page.dart';
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
